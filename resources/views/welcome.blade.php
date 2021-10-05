5b3838880396992b596d889a984410644af502af
badasukerubin <kelvinvip8@gmail.com>
2021-10-05 22:49:51 +0100

    added alpine and livewire

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="/css/app.css" rel="stylesheet">
+        <link href="{{mix('css/app.css')}}" rel="stylesheet">
+        <script src="{{ mix('js/app.js') }}" defer></script>
 
         <title>Laravel</title>
 
